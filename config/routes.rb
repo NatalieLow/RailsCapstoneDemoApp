f8a829c8d7f2f3fe195ebc8244581ab3612161a3
Natalie Low <gnatann@gmail.com>
2020-02-11 13:56:46 -0800

    added spa-demo assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'ui#index'	
-  
   scope :api, defaults: {format: :json}  do 
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
   end
+
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root 'ui#index'	
 end
