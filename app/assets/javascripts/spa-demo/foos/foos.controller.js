441139b20fd930ad7e65e4cf10effa54e9dc4dae
Natalie Low <gnatann@gmail.com>
2020-03-05 10:14:31 -0800

    working asset pipeline with CRUD foos object

--- app/assets/javascripts/spa-demo/foos/foos.controller.js
+++ app/assets/javascripts/spa-demo/foos/foos.controller.js
@@ -11,12 +11,17 @@
 		var vm = this;
 		vm.foos;
 		vm.foo;
+		vm.edit = edit;
+		vm.create = create;
+		vm.update = update;
+		vm.remove = remove;
 
 		activate();
 		return;
 
 		function activate(){
 			newFoo();
+			vm.foos = Foo.query();
 		}
 		function newFoo(){
 			vm.foo = new Foo();
@@ -24,19 +29,47 @@
 		function handleError(response){
 			console.log(response);
 		}
-		function edit(object, index){
+		function edit(object){
+			console.log("selected", object);
+			vm.foo = object;
 
 		}
+
 		function create(){
-
+			vm.foo.$save()
+			 .then(function(response){
+			 	// console.log(response);
+			 	vm.foos.push(vm.foo);
+			 })
+			 .catch(handleError);
 		}
+
 		function update(){
+			//console.log("update", vm.foo);
+			vm.foo.$update()
+			.then(function(response){
+				//console.log(response);
+			})
+			.catch(handleError);
 
 		}
 		function remove(){
-
+			vm.foo.$remove()
+			.then(function(response){
+				console.log(response);
+				// vm.foos = Foo.query();
+				removeElement(vm.foos, vm.foo);
+				newFoo();
+			})
+			.catch(handleError);
 		}
 		function removeElement(elements, element){
+			for (var i=0; i<elements.length; i++){
+				if(elements[i].id == element.id){
+					elements.splice(i,1);
+					break;
+				}
+			}
 
 		}
 	} 
